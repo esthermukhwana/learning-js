ae4b7ca7bd9e781e29c6b824abe93b2f63ea65f9
Esther mukwana <esthermkitui@gmail.com>
2018-02-17 12:43:27 +0300

    leaning js

---
+++ IfElse.js
@@ -0,0 +1,6 @@
+var issunday = false
+if (issunday) {
+	console.log("watch movie or sleep")
+} else {
+	console.log("learn code")
+}
---
+++ variables.js
@@ -0,0 +1,6 @@
+var classsize =23
+var is weekday = false;
+var classname = "mobiledevelopment"
+var shoesize = 5.5;
+// This is used to print
+console.log(classsize)
