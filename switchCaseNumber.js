1c466fbcc154cdf5e1287773ebded800edfe3d95
esthermukhwana <35222447+esthermukhwana@users.noreply.github.com>
2018-03-01 12:09:23 +0300

    Add files via upload

---
+++ switchCaseNumber.js
@@ -0,0 +1,29 @@
+var dayoftheweek= "Monday"
+
+switch(dayoftheweek){
+case "Monday":
+console.log("Today is Monday")
+break;
+
+case "Tuesday":
+console.log("Today is  Tuesday")
+break;
+case "Wednesday":
+console.log("Today is  Wednesday")
+break;
+case "Thursday":
+console.log("Today is  Thursday")
+break;
+case "Friday":
+console.log("Today is  Friday")
+break;
+case "Saturday":
+console.log("Today is  Saturday")
+break;
+case "Sunday":
+console.log("Today is  Sunday")
+break;
+
+default:("Error")
+break;
+}
