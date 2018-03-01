88ea405cc5975c52cb0b0eaa93b77a73b3f5b941
esthermukhwana <35222447+esthermukhwana@users.noreply.github.com>
2018-03-01 12:07:07 +0300

    Add files via upload

---
+++ switch case.js
@@ -0,0 +1,14 @@
+var isSunday = false
+
+switch(isSunday){
+case true:
+console.log("Today is sunday")
+break;
+
+case false:
+console.log("Today is not sunday")
+break;
+
+default:("Error")
+break;
+}
