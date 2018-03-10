24b14b8b15462a0db035f3e21c4fbfde2cdf8d0c
esthermukhwana <35222447+esthermukhwana@users.noreply.github.com>
2018-03-10 11:43:20 +0300

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,42 @@
+// this is a void function
+function add (){
+var numberone=10;
+var numbertwo=20;
+var sum = numberone + numbertwo;
+ console.log(sum);
+}
+// call the function
+add()
+function multiply(){
+	var numberone=10;
+	var numbertwo=20;
+	var numberthree=30;
+	var answer=numberone*numbertwo*numberthree;
+	return answer;
+
+}
+var result=multiply()
+console.log(result)
+
+
+
+
+function division(){
+	var numberone=1;
+	var numbertwo=2;
+	var divide=numberone/numbertwo;
+	return divide;
+}
+  var result=divide()
+  console.log(result)
+
+
+  function subtraction(){
+  	var numberone=3;
+  	var numbertwo=2;
+  	var subtraction=numberone-numbertwo;
+  	return subtraction;
+
+  }
+  var result=subtraction()
+  console.log(result)
