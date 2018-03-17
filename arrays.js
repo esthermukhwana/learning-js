d0259298b5b4b8b9f5991a845f37ece37c911192
esthermukhwana <35222447+esthermukhwana@users.noreply.github.com>
2018-03-17 09:17:10 +0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var student = ["jane","mary","lauren","ivy","fatma","judith","irene"]
+var shoesize=["5","40","32","42","30","44","28"]
+
+ // get the length of the array
+console.log(student.length)
+ for(i=0; i<=student.length;i++){
+ 	console.log(student[i]);
+ }
