88f3d4b6aefb285e29b1b84ae72856140a9fc497
esthermukhwana <35222447+esthermukhwana@users.noreply.github.com>
2018-06-23 08:40:48 +0300

    Add files via upload

---
+++ arrowfunction.js
@@ -0,0 +1,8 @@
+var addition=(x,y,z) => x+y+z;
+console.log(addition(1,2,3));
+
+var multiply=(a,b,c) => a+b+c;
+console.log(addition(9,6,2));
+
+var sabtract=(b,q,d) => b+q+d;
+console.log(addition(3,5,7));
